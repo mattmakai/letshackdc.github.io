ce4b7a222e74359bcdc88c006483293c0ce80fae
Matt Makai <makaimc@gmail.com>
2014-08-27 10:07:18 -0400

    working on companies model

--- source/settings.py
+++ source/settings.py
@@ -9,7 +9,7 @@ GITHUB_URL = 'https://github.com/makaimc/letshackdc.github.com'
 DISQUS_SITENAME = 'makaimc'
 PDF_GENERATOR = False
 
-DIRECT_TEMPLATES = ('index', 'sitemap', )
+DIRECT_TEMPLATES = ('index', 'sitemap', 'companies')
 
 SITEMAP_SAVE_AS = 'sitemap.xml'
 
